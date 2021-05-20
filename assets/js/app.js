f2c8a88e248c21581ca14542ef410756bd18f0e4
felipe <fm3209742@gmail.com>
2021-05-20 18:36:26 -0300

    debbuging

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
- const button = document.querySelector(".nav-button"),menu = document.querySelector(".menu"),closeButton = document.querySelector(".nav-button-close")
+ const button = document.querySelector(".nav-button"), menu = document.querySelector(".menu"),closeButton = document.querySelector(".nav-button-close")
  ,header = document.querySelector("header");
- button.onclick = () => menu.className = "menu-active";
- closeButton.onclick = () => menu.className = "menu";
- addEventListener('scroll', e => setTimeout(e.target.scrollingElement.scrollTop > 0 ? header.className = "active" : header.className = "", 1000));
+
+ button.onclick = () => menu.classList.add("active");
+ closeButton.onclick = () => menu.classList.remove("menu");
+
+ window.addEventListener('scroll', e => { 
+ 	setTimeout(e.target.scrollingElement.scrollTop > 0 ? (header.className = "active") : (header.className = "", 1000)) 
+ });
